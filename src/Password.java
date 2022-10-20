fd8a8bac8fa40e279ab5ac626729f675312177e2
Gavin Barnard <gbbarnard@oakland.edu>
2022-10-20 14:39:27 -0400

    first commit

---
+++ src/Password.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Password {
+ 
+    public static void main(String args[]) {
+
+        System.out.println("Please input your password: ");
+        
+        Scanner sc = new Scanner(System.in);
+        String password = sc.nextLine();
+        
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$";
+
+        boolean validPassword = isValidPassword(password, regex);
+        System.out.println("It's valid password: \n" + validPassword);
+
+    }
+ 
+    public static boolean isValidPassword(String password, String regex)
+    {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+}
